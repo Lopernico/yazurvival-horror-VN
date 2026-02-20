b81dc0fa9f7f56e26ad7c23e267ab6d80284ab19
Luis Fernando Aguilar Ortiz <79117692+Lopernico@users.noreply.github.com>
2026-02-19 22:00:35 -0700

    Delete survival novel directory

--- survival novel/dialogs/remove_pauses_oneoff.ps1
+++
@@ -1,9 +0,0 @@
-$path = 'c:\Users\Louie\Downloads\yazu14\survival novel\dialogs\script.json'
-$j = Get-Content $path -Raw | ConvertFrom-Json
-$map = @{}
-$j.nodes | Where-Object { $_.type -eq 'pause' } | ForEach-Object { $map[$_.id] = $_.next }
-foreach ($n in $j.nodes) { if ($n.next -and $map.ContainsKey($n.next)) { $n.next = $map[$n.next] } }
-$j.nodes = $j.nodes | Where-Object { $_.type -ne 'pause' }
-if ($map.ContainsKey($j.start)) { $j.start = $map[$j.start] }
-$j | ConvertTo-Json -Depth 100 | Set-Content -Encoding UTF8 $path
-Write-Output 'OK'
--- survival novel/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a minimal safe API to the renderer
-contextBridge.exposeInMainWorld('electronAPI', {
-  send: (channel, data) => {
-    const allowed = ['log'];
-    if (allowed.includes(channel)) ipcRenderer.send(channel, data);
-  }
-});
